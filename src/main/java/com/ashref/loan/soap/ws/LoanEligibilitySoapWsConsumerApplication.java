21e4f775ab5c4444446a50d9829f43278c55a452
Achref Bouadila <a_bouadila@live.fr>
2020-02-16 16:11:29 +0100

    first commit

---
+++ src/main/java/com/ashref/loan/soap/ws/LoanEligibilitySoapWsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.ashref.loan.soap.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanEligibilitySoapWsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanEligibilitySoapWsConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashref/loan/soap/ws/LoanEligibilitySoapWsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashref.loan.soap.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEligibilitySoapWsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
